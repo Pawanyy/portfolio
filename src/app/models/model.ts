7d9e4028e116358a67b1d4d4ef623edfb2fe2b4d
Pawanyy <pawanyyy01@gmail.com>
2023-04-04 17:25:50 +0530

    work experience done

--- src/app/models/model.ts
+++ src/app/models/model.ts
@@ -5,3 +5,12 @@ export interface Education {
     duration: string;
     score: string;
 }
+
+export interface WorkExperience {
+    role: string;
+    company: string;
+    duration: string;
+    description: string[];
+    contact: string[];
+}
+
--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WorkExperience } from '../models/model';
 
 @Component({
   selector: 'app-work-experience',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./work-experience.component.scss']
 })
 export class WorkExperienceComponent {
-
+  workExpList: WorkExperience[] = [
+    {
+      role: 'Software Developer',
+      company: 'Rudratech IT Services',
+      duration: 'Jan 2021 - Dec 2022',
+      description: [
+        'Developed hotel, B2B, and B2C solutions with Winforms and C#, as well as added thermal printing support.',
+        'Created MVPs for billing solutions, games, and mobile apps.',
+        'Developed an LMS website with features such as AWS integration for videos, social login, payment integration, and a referral system.'
+      ],
+      contact: ['Rahul Sachwani', '+91-7208344434']
+    }
+  ]
 }
