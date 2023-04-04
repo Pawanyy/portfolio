2842f94453d81efbd9a0c9be7c2085d823ec75a9
Pawanyy <pawanyyy01@gmail.com>
2023-04-05 00:05:27 +0530

    added meta tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+
+  constructor(private titleService:Title, private metaService:Meta) {}
+
+  ngOnInit(): void {
+    
+    this.titleService.setTitle('Pawan Yadav - Portfolio');
+
+    this.metaService.updateTag(
+      { name: 'description', content: 'Pawan Yadav Resume Website ' },
+    );
+    this.metaService.updateTag(
+      { name: 'keywords', content: 'Pawan Yadav, PawanYadav, YadavPawan, ' },
+    );
+    this.metaService.updateTag(
+      { name: 'language', content: 'English' },
+    );
+    this.metaService.updateTag(
+      { name: 'author', content: 'Pawan Pramod Yadav' },
+    );
+  }
 }
