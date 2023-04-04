c81260178769931960ea11ea1756588124e90f3f
Pawanyy <pawanyyy01@gmail.com>
2023-04-04 16:23:50 +0530

    added pi section data

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class PersonalInformationComponent {
 
+  myData: string[][] = [
+    ["Name", "Pawan Yadav"],
+    ["DOB", "10/09/1999"],
+    ["Work Exp", "2 Years"],
+    ["Education", "B.Sc. IT (2020)"],
+    ["Interests", "Reading, Chess, Video Game"]
+  ];
+
+  aboutMe: string[] = [
+    "Hi, I am Software Developer with 2 Years of Experience.",
+    "Hi, I am Software Developer with 2 Years of Experience.",
+    "Hi, I am Software Developer with 2 Years of Experience.",
+    "Hi, I am Software Developer with 2 Years of Experience.",
+    "Hi, I am Software Developer with 2 Years of Experience.",
+  ]
 }
