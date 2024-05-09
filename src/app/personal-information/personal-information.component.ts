9b322feba0e8b65149c3ff78078337e53251aaf5
Pawan Pramod Yadav <98683588+Pawanyy@users.noreply.github.com>
2024-05-09 05:41:22 +0000

    Updated experience

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -3,19 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personal-information',
   templateUrl: './personal-information.component.html',
-  styleUrls: ['./personal-information.component.scss']
+  styleUrls: ['./personal-information.component.scss'],
 })
 export class PersonalInformationComponent {
-
   myData: string[][] = [
-    ["Name", "Pawan Yadav"],
-    ["DOB", "10/09/1999"],
-    ["Work Exp", "2 Years"],
-    ["Education", "B.Sc. IT (2020)"],
-    ["Interests", "Reading, Chess, Video Game"]
+    ['Name', 'Pawan Yadav'],
+    ['DOB', '10/09/1999'],
+    ['Work Exp', '3 Years'],
+    ['Education', 'B.Sc. IT (2020)'],
+    ['Interests', 'Reading, Chess, Video Game'],
   ];
 
   aboutMe: string[] = [
-    "Hi, I'm a passionate software developer with 2 years of experience in C#, Java, PHP, JavaScript, Dot Net, Laravel, MySQL, Unity & WinForms with SQL Server. I enjoy creating user-friendly software solutions that meet clients' needs. I'm always looking for new challenges and opportunities to enhance my abilities.",
-  ]
+    "Hi, I'm a passionate software developer with 3 years of experience in C#, Java, PHP, JavaScript, Node.js, Dot Net, MySQL, Unity & WinForms with SQL Server. I enjoy creating user-friendly software solutions that meet clients' needs. I'm always looking for new challenges and opportunities to enhance my abilities.",
+  ];
 }
